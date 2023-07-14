063517c254d1cd3b9a2f6f6c2ff760ba8c8afd4a
falapea <falapea@unal.edu.co>
2023-07-14 17:19:27 -0500

    Se agregó el proyecto

---
+++ Project.py
@@ -0,0 +1,6 @@
+import pandas as pd # manejo de data frames
+import seaborn as sns # visualización
+import matplotlib.pyplot as plt
+import numpy as np #
+import io
+import scipy.stats # distribuciones estadísticas
