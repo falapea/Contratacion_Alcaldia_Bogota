8ffabdad90b2536e1d48c0f4f743da2a63832363
falapea <falapea@unal.edu.co>
2023-07-14 18:40:18 -0500

    Explorando los datos

--- Project.py
+++ Project.py
@@ -8,6 +8,11 @@ import numpy as np #
 import io
 import scipy.stats # distribuciones estadísticas
 
-data = pd.read_excel('AlcaldiaBogota_2017_2019.xslx' sheet_name="Datos")
+    # Agregamos la base de datos a través de la libreria pandas, la cual nos permite trabajar con archivos de excel 
+datos = pd.read_excel('C:\\Users\\Camilo\\Documents\\Data Science\\Alcaldia_projecto\\AlcaldiaBogota_2017_2019.xlsx')
+print(datos)
 
+    # Contrastamos y exploramos los datos de las columnas para verificar que esten todas las columnas correctamente cargadas
+columnas = datos.columns
+print = (columnas)
 
