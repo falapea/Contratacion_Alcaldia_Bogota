2c4c8549d045a7d6ae4ef0230d5fbd7d7f6fd602
falapea <falapea@unal.edu.co>
2023-07-14 17:29:00 -0500

    Verificando que github esté enlazado

--- Project.py
+++ Project.py
@@ -1,6 +1,13 @@
+
+    # Projecto de analitica de datos con una base de datos de libre acceso, detallare el paso a paso de este análisis
+
 import pandas as pd # manejo de data frames
 import seaborn as sns # visualización
 import matplotlib.pyplot as plt
 import numpy as np #
 import io
 import scipy.stats # distribuciones estadísticas
+
+data = pd.read_excel('AlcaldiaBogota_2017_2019.xslx' sheet_name="Datos")
+
+
